48fc73a950371d26b2194651e77c7bc0ca4c7218
baiyibonbbb <614503145@qq.com>
2026-03-16 18:17:18 +0800

    222

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.delete('/delete/:filename', (req, res) => {
 });
 
 // 启动服务器
-app.listen(port, () => {
-    console.log(`服务器运行在 http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`服务器运行在 http://0.0.0.0:${port}`);
 });
